a5274814cfb7d522851e60bc8a22cec2050344ce
Nebelhom <nebelhom@hotmail.com>
2017-11-01 16:35:12 +0100

    download_all and download established, added since_id to feed.csv
    download_all and download methods established (https://gist.github.com/yanofsky/5436496 as reference)
    Added since_id to feed.csv for easy handle to download only the latest tweets and to update tweets collected.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!usr/bin/env python
 
 import csv
-from tweepy import OAuthHandler, API
+import time
+import tweepy
+#from tweepy import OAuthHandler, API
 
 """
 Purpose: Download tweets from interesting feeds. classify them into interesting and not interesting
@@ -56,8 +58,8 @@ class TweetDownloader(object):
         results = []
         with open(fname, 'r') as f:
             reader = csv.reader(f)
-            for row in reader:
-                results.append(row[0])
+            for feed, since_id in reader:
+                results.append((feed, since_id))
 
         return results
 
@@ -83,15 +85,44 @@ class TweetDownloader(object):
 
         return results
 
-    def download_all_tweets(self, feed):
-        auth = OAuthHandler(self.ckey, self.csecret)
+    def download(self, feed, count=200, since_id=None, exclude_replies=True, include_rtf=True):
+        auth = tweepy.OAuthHandler(self.ckey, self.csecret)
         auth.set_access_token(self.atoken, self.asecret)
-        api = API(auth)
+        api = tweepy.API(auth)
+
+        # https://developer.twitter.com/en/docs/tweets/timelines/guides/working-with-timelines
+        # Use as reference
 
         # from
+        # https://gist.github.com/yanofsky/5436496
+        # and
         # https://stackoverflow.com/questions/45867934/is-it-possible-to-download-tweets-and-retweets-of-10000-users-with-tweepy-throug
         alltweets = []
-        new_tweets = api.user_timeline(screen_name = feed, count=200) 
+        if since_id:
+            new_tweets = api.user_timeline(screen_name=feed, count=count,
+                since_id=since_id, exclude_replies=exclude_replies,
+                include_rtf=include_rtf)
+        else:
+            new_tweets = api.user_timeline(screen_name=feed, count=count,
+                exclude_replies=exclude_replies, include_rtf=include_rtf)
+
+        # save most recent tweets
+        alltweets.extend(new_tweets)
+
+        return alltweets
+
+    def download_all(self, feed, exclude_replies=True, include_rtf=True):
+        auth = tweepy.OAuthHandler(self.ckey, self.csecret)
+        auth.set_access_token(self.atoken, self.asecret)
+        api = tweepy.API(auth)
+
+        # from
+        # https://gist.github.com/yanofsky/5436496
+        # and
+        # https://stackoverflow.com/questions/45867934/is-it-possible-to-download-tweets-and-retweets-of-10000-users-with-tweepy-throug
+        alltweets = []
+        new_tweets = api.user_timeline(screen_name=feed, count=200,
+            exclude_replies=exclude_replies, include_rtf=include_rtf)
 
         # save most recent tweets
         alltweets.extend(new_tweets)
@@ -99,17 +130,52 @@ class TweetDownloader(object):
         # save the id of the oldest tweet less one
         oldest = alltweets[-1].id - 1
 
-        #keep grabbing tweets until there are no tweets left to grab
-        #while new_tweets:       
-        #    try:
-        #        new_tweets = api.user_timeline(screen_name = feed,count=200,max_id=oldest)
-        #    except tweepy.TweepError:
-        #        time.sleep(60 * 15)
-        #        continue
+        # keep grabbing tweets until there are no tweets left to grab
+        while len(new_tweets) > 0:
+            print("getting tweets before {}".format(oldest))
+            
+            #all subsiquent requests use the max_id param to prevent duplicates
+            new_tweets = api.user_timeline(screen_name=feed, count=200,
+            exclude_replies=exclude_replies, include_rtf=include_rtf)
+            
+            #save most recent tweets
+            alltweets.extend(new_tweets)
+            
+            #update the id of the oldest tweet less one
+            oldest = alltweets[-1].id - 1
+            
+            print ("...{} tweets downloaded so far".format(len(alltweets)))
 
         return alltweets
 
+    def toCSV(self, tweets, feed):
+        #transform the tweepy tweets into a 2D array that will populate the csv 
+        outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in tweets]
+        
+        #write the csv  
+        with open('{}_tweets.csv'.format(feed), 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(["id","created_at","text"])
+            writer.writerows(outtweets)
+
+
 if __name__ == '__main__':
     tw = TweetDownloader()
-    tweets = tw.download_all_tweets(tw.feeds[0])
-    print(tweets[0])
+    for feed, since_id in tw.feeds:
+        if since_id == '-1':
+            tweets = tw.download(feed)
+        else:
+            tweets = tw.download(feed, since_id=since_id)
+        tw.toCSV(tweets, feed)
+    #print(dir(tweets[0]))
+    #print('\n')
+    print(len(tweets))
+    #print('\n')
+    #for status in tweets:
+        #print(status, '\n')
+        #print('Tweet: ', status.text, '\n')
+        #print('Author: ', type(status.user), '\n')
+        #print('JSON\n')
+        #for item in status._json:
+        #    print(item, '\n')
+        #print('========================\n\n')
