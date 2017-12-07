526d42abee789343713afdc0b14ca494e9e59bcb
Nebelhom <nebelhom@hotmail.com>
2017-12-07 12:33:07 +0100

    Internalised all methods

--- main.py
+++ main.py
@@ -25,60 +25,6 @@ Later try to classify automatically into separate categories
 At the very end search large twitter space to look for fitting tweets across whole space with trained model
 """
 
-def text_process(mess):
-    """
-    Takes in a string of text, then performs the following:
-    1. Remove all punctuation
-    2. Remove all stopwords
-    3. Returns a list of the cleaned text
-    """
-    # Check characters to see if they are in punctuation
-    nopunc = [char for char in mess if char not in string.punctuation + "–"]
-
-    # Check if char is an emoji
-    noemoji = [char for char in nopunc if char not in emoji.UNICODE_EMOJI.keys()]
-
-    # Join the characters again to form the string.
-    nopunc = ''.join(noemoji)
-
-    # Now just remove any stopwords
-    return [word for word in nopunc.split() if word.lower() not in stopwords.words('english') if 'http' not in word.lower()]
-
-def get_tweet_text_full_text_tuple(tweet):
-    """
-    Finds full text of the tweet. No matter if normal length, extended or
-    retweeted.
-    """
-    full_text = ""
-
-    if 'retweeted_status' in tweet._json:
-        full_text = tweet._json['retweeted_status']['full_text']
-
-    elif "extended_tweet" in tweet._json and "full_text" in tweet._json["extended_tweet"]:
-        full_text = tweet._json["extended_tweet"]["full_text"]
-
-        if "display_text_range" in tweet._json["extended_tweet"]:
-            beginIndex = tweet._json["extended_tweet"]["display_text_range"][0]
-            endIndex   = tweet._json["extended_tweet"]["display_text_range"][1]
-            full_text = full_text[beginIndex:endIndex]
-
-    elif "full_text" in tweet._json:
-        full_text = tweet._json["full_text"]
-
-        if "display_text_range" in tweet._json:
-            beginIndex = tweet._json["display_text_range"][0]
-            endIndex   = tweet._json["display_text_range"][1]
-            full_text = full_text[beginIndex:endIndex]
-
-    elif "text" in tweet._json:
-        text = tweet._json["text"]
-        full_text = text
-    else:
-        text = tweet.text
-        full_text = text
-
-    return full_text
-
 class TweetCollector(object):
     """
     Class written to download tweets from all feeds specified and then save them in a csv file... or sql (make it sql for fun)
@@ -98,7 +44,8 @@ class TweetCollector(object):
         self.feeds = pd.read_csv(feedfile)
         self.columns = ['since_id', 'created_at', 'tweet', 'feed']
 
-    def download(self):
+    @property
+    def tweets(self):
         """
         Download and accumulate tweets from multiple feeds
         """
@@ -151,53 +98,111 @@ class TweetCollector(object):
         #transform the tweepy tweets into a 2D array that will populate the csv
 
         outtweets = [[tweet.id_str, tweet.created_at,
-                      get_tweet_text_full_text_tuple(tweet), feed]
+                      self.get_tweet_full_text(tweet), feed]
                      for tweet in alltweets]
 
         df_tweets = pd.DataFrame(data=outtweets, columns=self.columns)
 
         return df_tweets
 
-    def to_CSV(self, tweets, csvname=''):
+    def to_CSV(self, csvname=''):
         if csvname == '':
             csvname = 'Tweets.csv'
-        tweets.to_csv(csvname, index=False)
+        self.tweets.to_csv(csvname, index=False)
 
-    def to_XLS(self, tweets, xlsname=''):
+    def to_XLS(self, xlsname=''):
         if xlsname == '':
             xlsname = 'Tweets.xlsx'
         # Create a Pandas Excel writer using XlsxWriter as the engine.
         writer = pd.ExcelWriter(xlsname, engine='xlsxwriter')
 
         # Convert the dataframe to an XlsxWriter Excel object.
-        tweets.to_excel(writer, sheet_name='Sheet1', index=False)
+        self.tweets.to_excel(writer, sheet_name='Sheet1', index=False)
 
         # Close the Pandas Excel writer and output the Excel file.
         writer.save()
 
-    def save_feeds_csv(self, db, fname='feeds.csv'):
+    def get_tweet_full_text(self, tweet):
+        """
+        Finds full text of the tweet. No matter if normal length, extended or
+        retweeted.
+        """
+        full_text = ""
+
+        if 'retweeted_status' in tweet._json:
+            full_text = tweet._json['retweeted_status']['full_text']
+
+        elif "extended_tweet" in tweet._json and "full_text" in tweet._json["extended_tweet"]:
+            full_text = tweet._json["extended_tweet"]["full_text"]
+
+            if "display_text_range" in tweet._json["extended_tweet"]:
+                beginIndex = tweet._json["extended_tweet"]["display_text_range"][0]
+                endIndex   = tweet._json["extended_tweet"]["display_text_range"][1]
+                full_text = full_text[beginIndex:endIndex]
+
+        elif "full_text" in tweet._json:
+            full_text = tweet._json["full_text"]
+
+            if "display_text_range" in tweet._json:
+                beginIndex = tweet._json["display_text_range"][0]
+                endIndex   = tweet._json["display_text_range"][1]
+                full_text = full_text[beginIndex:endIndex]
+
+        elif "text" in tweet._json:
+            text = tweet._json["text"]
+            full_text = text
+        else:
+            text = tweet.text
+            full_text = text
+
+        return full_text
+
+    def text_process(self, mess):
+        """
+        Takes in a string of text, then performs the following:
+        1. Remove all punctuation
+        2. Remove all stopwords
+        3. Returns a list of the cleaned text
+        """
+        # Check characters to see if they are in punctuation
+        nopunc = [char for char in mess if char not in string.punctuation + "–"]
+
+        # Check if char is an emoji
+        noemoji = [char for char in nopunc if char not in emoji.UNICODE_EMOJI.keys()]
+
+        # Join the characters again to form the string.
+        nopunc = ''.join(noemoji)
+
+        # Now just remove any stopwords
+        return [word for word in nopunc.split() if word.lower() not in stopwords.words('english') if 'http' not in word.lower()]
+
+    def clean_tweet_col(self, new_col="clean_tweet"):
+        """
+        Creates a new column in the pandas dataframe with
+        precursor to bag of words using text_process method
+        """
+        self.tweets[new_col] = self.tweets['tweet'].apply(self.text_process)
+
+    def save_feeds_csv(self, fname='feeds.csv'):
         """
         Saves a list of feeds and also the oldest since_id
 
         Steps: identify all tweet feeds in db, find the oldest of each feed in that list
         """
-        feeds = db['feed'].unique()
+        feeds = self.tweets['feed'].unique()
+        print(feeds, '\n')
         result = []
         for f in feeds:
             result.append([
                 f,
-                db[db['feed'] == f]['since_id'].max()
+                self.tweets[self.tweets['feed'] == f]['since_id'].max()
                 ])
         new_feed = pd.DataFrame(data=result, columns=['feed', 'since_id'])
         new_feed.to_csv(fname, index=False)
 
-    def add_remove_punct(self, tweets):
-        tweets['tweet'] = tweets['tweet'].apply(text_process)
-
 if __name__ == '__main__':
     tw = TweetCollector(feedfile='feeds.csv')
-    tweets = tw.download()
-    tweets['clean_tweet'] = tweets['tweet'].apply(text_process)
-    tw.to_CSV(tweets)
-    tw.to_XLS(tweets)
-    tw.save_feeds_csv(tweets, fname='test_feeds.csv')
+    tw.clean_tweet_col()
+    tw.to_CSV()
+    tw.to_XLS()
+    tw.save_feeds_csv(fname='test_feeds.csv')
