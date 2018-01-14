25efeb898ac034aff35715333462a7810d03b1f6
Nebelhom <nebelhom@hotmail.com>
2018-01-14 22:39:46 +0100

    Add config.py

---
+++ user_data/config.py
@@ -0,0 +1,10 @@
+#!usr/bin/python
+
+import os
+import os.path as osp
+
+env_args = {
+	'feedfile': osp.join('user_data', 'feed.csv')
+	'keyfile': osp.join('user_data', 'keys.csv')
+	'train': 'training_data.xlsx'
+}
